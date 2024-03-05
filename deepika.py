f9458a16cea70e9f9e4bb40696ac219fbd1ed1cc
Dhinakaran <dhina15003@gmail.com>
2024-03-05 17:48:52 +0530

    cvbomn

--- deepika.py
+++ deepika.py
@@ -1,20 +1,3 @@
-def fibonacci(n):
-    fib_series = [0, 1]  # Initialize the Fibonacci series with the first two numbers
-
-    # Generate the Fibonacci series
-    for i in range(2, n):
-        next_num = fib_series[-1] + fib_series[-2]
-        fib_series.append(next_num)
-
-    return fib_series
-
-
-# Test the function
-n = int(input("Enter the number of terms for Fibonacci series: "))
-print(f"Fibonacci series with {n} terms:")
-print(fibonacci(n))
-print(asdfg)
-print(2345t6y)print(dhinakaran)
-print("this is dhinakaran")
-print("i am getting scared")
-print("now feel comfort")
+a  =[[1,8,9],[4,5,7],[7,6,4]]
+print(min(sublist(a)))
+g
