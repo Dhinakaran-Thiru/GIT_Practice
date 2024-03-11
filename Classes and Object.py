c2cb15f5f3096423b00768ed1d3dcac4b0c9e986
Dhinakaran <dhina15003@gmail.com>
2024-03-11 11:19:11 +0530

    commited classes and object

---
+++ Classes and Object.py
@@ -0,0 +1,40 @@
+"""class goa:
+    drink = "drinking"
+    name = "dhina"
+    def party(self):
+        print("lets party")
+    def beach(self):
+        print("Enjoying the beach")
+ramesh=goa()
+suresh=goa()
+ramesh.party()
+suresh.beach()
+ramesh.drink="yes"
+suresh.drink="no"
+ramesh.name="ramesh"
+print(ramesh.name)
+print(suresh.name)
+print(ramesh.drink)
+print(suresh.drink)"""
+
+
+
+class laptop:
+    price=""
+    Processor=""
+    Ram=""
+
+hp=laptop()
+dell=laptop()
+
+hp.price=50000
+hp.Processor="i5"
+hp.Ram="26gb"
+dell.price=60000
+dell.Processor="i5ig"
+dell.Ram="26g280b"
+
+print(dell.price)
+
+print(hp.price)
+
