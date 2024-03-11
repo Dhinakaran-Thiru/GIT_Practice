a5d95ade0c1603592833bf1958de8577df5e255a
Dhinakaran <dhina15003@gmail.com>
2024-03-11 15:02:59 +0530

    practiced the oops concept

--- Classes and Object.py
+++ Classes and Object.py
@@ -19,7 +19,7 @@ print(suresh.drink)"""
 
 
 
-class laptop:
+"""class laptop:
     price=""
     Processor=""
     Ram=""
@@ -36,5 +36,70 @@ dell.Ram="26g280b"
 
 print(dell.price)
 
-print(hp.price)
+print(hp.price)"""
+"""class laptop:
+    def __init__(self):
+        self.price=100
+        self.ram="i5"
+        self.processor="fkfi"
+hp=laptop()
+print()"""
+
+"""l1=[3,45,68,845,34]
+l2=[34,4,24,53,242,34]
+l3=[]
+for i in l1:
+    if (i%2==0):
+        l3.append(i)
+for j in l2:
+    if (j%2!=0):
+        l3.append(j)
+print(l3)"""
+
+"""class car:
+    name="luxury car"
+    def __init__(self):
+        self.price="59999"
+        self.processor="dhina"
+laxmi=car()
+print(laxmi.price)
+print(laxmi.processor)"""
+
+"""class student:
+    def __init__(self):
+        self.name="dhinakaran"
+        self.regno=111420104019
+    def display(self):
+        print("name",self.name)
+        print("regno",self.regno)
+
+s1=student()
+print(s1.name)
+print(s1.regno)
+s1.display()"""
+"""class fruit:
+    def __init__(self):
+        self.color="black"
+apple=fruit()
+apple.color="red"
+print(apple.color)"""
+
+class student:
+    def __init__(self,brand,cost):
+        self.brand=brand
+        self.cost=cost
+    def display(self):
+        print(self.brand)
+        print(self.cost)
+
+samsung=student("iphone",7484930)
+samsung.display()
+
+
+
+
+
+
+
+
 
